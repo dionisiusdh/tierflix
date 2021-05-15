f03002a5df0cbf1a0d90b6713995b92db0fda65f
Dionisius Darryl Hermansyah <dionisius.dh28@gmail.com>
2021-05-15 21:01:17 +0800

    feat(dnd): fix reordering

--- src/MovieList.tsx
+++ src/MovieList.tsx
@@ -21,55 +21,54 @@ export const MovieList: React.FC<Props> = ({ listId, listType, row }) => {
       >
         {(dropProvided) => (
           <div {...dropProvided.droppableProps}>
-            <div>
-              <div
-                style={{
-                  display: "flex",
-                  backgroundColor: "pink",
-                  margin: "20px",
-                  minHeight: "85px",
-                  overflowX: "scroll",
-                }}
-                ref={dropProvided.innerRef}
-              >
-                {row.urls.map((url, index) => (
-                  <Draggable key={url} draggableId={url} index={index}>
-                    {(dragProvided) => (
-                      <div
-                        {...dragProvided.dragHandleProps}
-                        {...dragProvided.draggableProps}
-                        ref={dragProvided.innerRef}
-                      >
-                        <img
-                          style={{
-                            width: "130px",
-                            height: "80px",
-                            marginRight: "5px",
-                          }}
-                          src={url}
-                        />
-                      </div>
-                    )}
-                  </Draggable>
-                ))}
-                {dropProvided.placeholder}
-              </div>
+            <div
+              style={{
+                display: "flex",
+                backgroundColor: "pink",
+                margin: "20px",
+                minHeight: "85px",
+                overflowX: "auto",
+              }}
+              ref={dropProvided.innerRef}
+            >
+              {row.urls.map((url, index) => (
+                <Draggable key={url} draggableId={url} index={index}>
+                  {(dragProvided) => (
+                    <div
+                      {...dragProvided.dragHandleProps}
+                      {...dragProvided.draggableProps}
+                      ref={dragProvided.innerRef}
+                    >
+                      <img
+                        style={{
+                          width: "130px",
+                          height: "80px",
+                          marginRight: "5px",
+                        }}
+                        src={url}
+                      />
+                    </div>
+                  )}
+                </Draggable>
+              ))}
+              {dropProvided.placeholder}
             </div>
           </div>
         )}
       </Droppable>
       <style>
         {`::-webkit-scrollbar {
-            width: 1em;
+            width: .1rem;
+            height: .5rem;
           }
           
           ::-webkit-scrollbar-track {
-            box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+            box-shadow: inset 0 0 2px rgba(0, 0, 0, 0.3);
           }
           
           ::-webkit-scrollbar-thumb {
             background-color: darkgrey;
-            outline: 1px solid slategrey;
+            border-radius: 10px;
           }`}
       </style>
     </>
--- src/reorder.ts
+++ src/reorder.ts
@@ -1,5 +1,5 @@
 import { DraggableLocation } from "react-beautiful-dnd";
-import { ColorMap, Row } from "./types";
+import { Row } from "./types";
 
 // a little function to help us with reordering the result
 export const reorder = (
@@ -19,26 +19,37 @@ export const reorderRows = (
   source: DraggableLocation,
   destination: DraggableLocation
 ) => {
-  return rows;
-  // const current = [...colors[source.droppableId]];
-  // const next = [...colors[destination.droppableId]];
-  // const target = current[source.index];
-  // // moving to same list
-  // if (source.droppableId === destination.droppableId) {
-  //   const reordered = reorder(current, source.index, destination.index);
-  //   return {
-  //     ...colors,
-  //     [source.droppableId]: reordered,
-  //   };
-  // }
-  // // moving to different list
-  // // remove from original
-  // current.splice(source.index, 1);
-  // // insert into next
-  // next.splice(destination.index, 0, target);
-  // return {
-  //   ...colors,
-  //   [source.droppableId]: current,
-  //   [destination.droppableId]: next,
-  // };
+  const current = rows.find((x) => x.id === source.droppableId)!;
+  const next = rows.find((x) => x.id == destination.droppableId)!;
+  const target = current.urls[source.index];
+
+  // moving to same list
+  if (source.droppableId === destination.droppableId) {
+    const reordered = reorder(current.urls, source.index, destination.index);
+    return rows.map((x) =>
+      x.id === current.id ? { ...x, urls: reordered } : x
+    );
+  }
+
+  // moving to different list
+  // remove from original
+  current.urls.splice(source.index, 1);
+  // insert into next
+  next.urls.splice(destination.index, 0, target);
+
+  return rows.map((x) => {
+    if (current.id === x.id) {
+      return {
+        ...x,
+        urls: current.urls,
+      };
+    } else if (next.id === x.id) {
+      return {
+        ...x,
+        urls: next.urls,
+      };
+    }
+
+    return x;
+  });
 };
